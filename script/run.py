42e1ed93a67a07f6c48acaf2270436a40a194f42
yidi <yidi.huang.elodie@gmail.com>
2024-02-26 22:50:26 +0100

    update run.py

--- script/run.py
+++ script/run.py
@@ -14,7 +14,7 @@ def main():
         elif command == "store":
             commande11=['python3','store_char.py','Gloss1.txt']
             commande12=['python3','store_word2.py','Gloss2.txt']
-            commande13=['python3','store_word3.py','Gloss2.txt']
+            commande13=['python3','store_word3.py','Gloss3.txt']
             consource = input("Entrez le conll que vous voulez appliquer, il doit se trouver dans le dossier conll (ex. chinese-beginner.A1.mSUD.conllu) :")
             commande11.append(consource)
             commande12.append(consource)
@@ -23,8 +23,8 @@ def main():
             subprocess.run(commande12,check=True)
             subprocess.run(commande13,check=True)
             subprocess.run(['python3','store.py'],check=True)
-
             print('Vos dicos se trouveront dans le dossier dico.')
+            print('\n')
 
         elif command == "attach":
             commande2=['python3','attach.py']
